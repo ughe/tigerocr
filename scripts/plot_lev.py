5c050eee19404b1034f6ff821e32dd62ba2a188f
William Ughetta <wughetta@princeton.edu>
2021-02-16 01:13:15 -0500

    Plot Character Error Rates given a results.csv file

--- scripts/plot_lev.py
+++ scripts/plot_lev.py
@@ -4,6 +4,17 @@ import os
 import sys
 import webbrowser
 
+# BEGIN configuration
+providers = ["aws", "azu", "gcp"]
+metrics = ["cer", "millis"]
+convert = [
+        lambda x: min(max(float(x), 0), 1),
+        lambda x: min(max(int(x)/1000, 0), 12),
+]
+providersAxis = ["AWS", "Azure", "GCP"]
+metricsAxis = ["CER", "Seconds"]
+# END configuration
+
 def save(fig, fname, fopen=False):
     fig.savefig(fname, bbox_inches="tight")
     if fopen:
@@ -11,32 +22,44 @@ def save(fig, fname, fopen=False):
 
 def init_ax(title, ax):
     ax.set_xlabel("Image Pointer")
-    ax.set_ylabel("Levenshtein Distance")
+    ax.set_ylabel("CER")
     ax.set_title(title)
 
-def plot_cer(fname, outf):
+def plot_cer(fname, outf, metricIndex):
     data = [x.split(",") for x in open(fname, "r").read().strip().split("\n")]
-    aws = [(int(x[1]), (int(x[1+1])/1000)) for x in data]
-    azu = [(int(x[3]), (int(x[3+1])/1000)) for x in data]
-    gcp = [(int(x[5]), (int(x[5+1])/1000)) for x in data]
+    res = {}
+    for m in metrics:
+        res[m] = {}
+    for row in data:
+        lbl = row[0].lower()
+        for i, m in enumerate(metrics):
+            c = convert[i]
+            if m in lbl:
+                for p in providers:
+                    if p in lbl:
+                        res[m][p] = [c(x) for x in row[1:]]
+
+    mi = metricIndex
+    m = metrics[mi]
 
     fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, ncols=1, figsize=(8, 9), dpi=300)
-    init_ax("AWS vs Azure", ax0)
-    init_ax("Azure vs GCP", ax1)
-    init_ax("GCP vs AWS", ax2)
-    fig.suptitle("OCR Levenshtein Distances: %d Images" % len(data), size="xx-large", y=1.01)
+    init_ax(providersAxis[0] + " " + metricsAxis[mi], ax0)
+    init_ax(providersAxis[1] + " " + metricsAxis[mi], ax1)
+    init_ax(providersAxis[2] + " " + metricsAxis[mi], ax2)
+    fig.suptitle("OCR %s: %d Images" % (metricsAxis[mi], len(res[m][providers[0]])), size="xx-large", y=1.01)
     fig.tight_layout()
-    x = list(range(len(aws)))
-    ax0.scatter(x, [x[0] for x in aws], s=1, color="orange", label="AWS")
-    ax1.scatter(x, [x[0] for x in azu], s=1, color="blue", label="Azure")
-    ax2.scatter(x, [x[0] for x in gcp], s=1, color="red", label="GCP")
+    x = list(range(len(res[m][providers[0]])))
+    ax0.scatter(x, res[m][providers[0]], s=1, color="orange", label=providersAxis[0])
+    ax1.scatter(x, res[m][providers[1]], s=1, color="blue", label=providersAxis[1])
+    ax2.scatter(x, res[m][providers[2]], s=1, color="red", label=providersAxis[2])
     save(fig, outf)
     plt.close()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("usage: ./plot_lev.py combined.csv out.png")
+    if len(sys.argv) != 4:
+        print("usage: ./plot_lev.py results.csv out.png 0")
         sys.exit(1)
     fname = sys.argv[1]
     outf = sys.argv[2]
-    plot_cer(fname, outf)
+    metricI = int(sys.argv[3])
+    plot_cer(fname, outf, metricI)
--- scripts/plot_lev_vs_time.py
+++ scripts/plot_lev_vs_time.py
@@ -7,23 +7,51 @@ import os
 import sys
 import webbrowser
 
+# BEGIN configuration
+providers = ["aws", "azu", "gcp"]
+metrics = ["cer", "millis"]
+convert = [
+        lambda x: min(max(float(x), 0), 1),
+        lambda x: min(max(int(x)/1000, 0), 12),
+]
+providersAxis = ["AWS", "Azure", "GCP"]
+metricsAxis = ["CER", "Seconds"]
+# END configuration
+
 def save(fig, fname, fopen=False):
     fig.savefig("" + fname, bbox_inches="tight")
     if fopen:
         webbrowser.open_new("file://" + os.path.abspath("" + fname))
 
-def init_ax(ax, max_time, max_dist):
+def init_ax(ax, title, max_time=12, max_dist=1):
     ax.set_xlim(max_dist, 0)
     ax.set_ylim(0, max_time)
     ax.set_yticks(range(0, max_time+1, 3))
     ax.yaxis.set_minor_locator(AutoMinorLocator(2))
     ax.xaxis.set_minor_locator(AutoMinorLocator(2))
+    ax.set_title(title)
 
-def main(fname, outf, max_time, max_dist):
+def main(fname, outf):
     data = [x.split(",") for x in open(fname, "r").read().strip().split("\n")]
-    aws = [(min(int(x[1]), max_dist), min(int(x[1+1])/1000, max_time)) for x in data]
-    azu = [(min(int(x[3]), max_dist), min(int(x[3+1])/1000, max_time)) for x in data]
-    gcp = [(min(int(x[5]), max_dist), min(int(x[5+1])/1000, max_time)) for x in data]
+    res = {}
+    for m in metrics:
+        res[m] = {}
+    for row in data:
+        lbl = row[0].lower()
+        for i, m in enumerate(metrics):
+            c = convert[i]
+            if m in lbl:
+                for p in providers:
+                    if p in lbl:
+                        res[m][p] = [c(x) for x in row[1:]]
+
+    m0, m1 = metrics[0], metrics[1]
+    ps = sorted(providers)
+    size = len(res[m0][ps[0]])
+    p0 = [res[m0][ps[0]], res[m1][ps[0]]]
+    p1 = [res[m0][ps[1]], res[m1][ps[1]]]
+    p2 = [res[m0][ps[2]], res[m1][ps[2]]]
+
     fig = plt.figure(figsize=(8, 6), dpi=300, constrained_layout=True)
     spec = gridspec.GridSpec(ncols=2, nrows=3, height_ratios=[1, 1, 1],
             width_ratios=[4, 1], figure=fig)
@@ -32,34 +60,29 @@ def main(fname, outf, max_time, max_dist):
     ax2 = fig.add_subplot(spec[2, 1])
     ax = fig.add_subplot(spec[0:, 0])
     
-    ax.set_title("Pairwise Levenshtein vs. Time: %d Images" % len(data), size="xx-large")
-    init_ax(ax, max_time, max_dist)
-    init_ax(ax0, max_time, max_dist)
-    init_ax(ax1, max_time, max_dist)
-    init_ax(ax2, max_time, max_dist)
-    ax0.scatter(*zip(*aws), color="orange", s=.001, label="AWS vs Azure")
-    ax1.scatter(*zip(*azu), color="blue", s=.001, label="Azure vs GCP")
-    ax2.scatter(*zip(*gcp), color="red", s=.001, label="GCP vs AWS")
-    ax0.set_title("AWS vs Azure")
-    ax1.set_title("Azure vs GCP")
-    ax2.set_title("GCP vs AWS")
+    init_ax(ax, "")
+    ax.set_title("%s vs. %s: %d Images" % (metricsAxis[0], metricsAxis[1], size), size="xx-large")
+    init_ax(ax0, providersAxis[0])
+    init_ax(ax1, providersAxis[1])
+    init_ax(ax2, providersAxis[2])
+    ax0.scatter(*p0, color="orange", s=.001, label=providersAxis[0])
+    ax1.scatter(*p1, color="blue", s=.001, label=providersAxis[1])
+    ax2.scatter(*p2, color="red", s=.001, label=providersAxis[2])
     
     ax.grid(linewidth=1, alpha=0.2)
-    ax.set_xlabel("Pairwise Levenshtein Distance")
-    ax.set_ylabel("Time [Seconds]")
-    ax.scatter(*zip(*aws), color="orange", s=1, label="AWS vs Azure")
-    ax.scatter(*zip(*azu), color="blue", s=1, label="Azure vs GCP")
-    ax.scatter(*zip(*gcp), color="red", s=1, label="GCP vs AWS")
+    ax.set_xlabel(metricsAxis[0])
+    ax.set_ylabel(metricsAxis[1])
+    ax.scatter(*p0, color="orange", s=1, label=providersAxis[0])
+    ax.scatter(*p1, color="blue", s=1, label=providersAxis[1])
+    ax.scatter(*p2, color="red", s=1, label=providersAxis[2])
     
     plt.close()
     save(fig, outf)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("usage: ./plot_lev_vs_time.py combined.csv out.png max_time max_dist")
+    if len(sys.argv) != 3:
+        print("usage: ./plot_lev_vs_time.py results.csv out.png")
         sys.exit(1)
     fname = sys.argv[1]
     outf = sys.argv[2]
-    max_time = int(sys.argv[3])
-    max_dist = int(sys.argv[4])
-    main(fname, outf, max_time, max_dist)
+    main(fname, outf)
